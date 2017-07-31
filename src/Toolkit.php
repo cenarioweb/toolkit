0dc33cd549b6407d152d63150b2a5618af616c7d
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2017-07-31 01:34:54 -0300

    Eliminando redundância

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -8,29 +8,4 @@ class Toolkit
     {
         //
     }
-
-    public static function capitular($str)
-    {
-        $words = explode(' ', mb_strtolower(trim(preg_replace("/\s+/", ' ', $str))));
-        $return[] = ucfirst($words[0]);
-
-        unset($words[0]);
-
-        foreach ($words as $word) {
-            if (!preg_match("/^([dn]?[aeiou][s]?|em)$/i", $word)) {
-                $word = ucfirst($word);
-            }
-
-            $return[] = $word;
-        }
-        $text = implode(' ', $return);
-        $text = str_replace(' Ti', ' TI', $text);
-
-        return $text;
-    }
-
-    public static function numbersOnly($str)
-    {
-        return preg_replace('/[^\d]/', '', $str);
-    }
 }
